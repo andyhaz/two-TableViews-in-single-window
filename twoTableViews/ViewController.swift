308f31b8ea986915b3157cdff135bed1edcf72af
andrew <andyhaz1970@gmail.com>
2023-05-23 16:09:06 -0500

    Initial Commit

---
+++ twoTableViews/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  twoTableViews
+//
+//  Created by andyhaz on 5/23/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
